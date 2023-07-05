816b7ebfd61706067b63c1f6b4941a93e6089476
Vaitul Bhayani <vaitulb@gmail.com>
2023-07-05 17:41:52 +0530

    fix: fixed log message added server running link there

--- server.ts
+++ server.ts
@@ -26,5 +26,5 @@ app.use(OTHER_ROUTES);
 
 //STARTING SERVER
 app.listen(SERVER_CONST.PORT, () =>
-  console.log("Server is running on port " + SERVER_CONST.PORT)
+  console.log("Server is running at http://localhost:" + SERVER_CONST.PORT)
 );

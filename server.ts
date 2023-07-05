1b1b4ffcaf19b05c6ade850f4335342ed30c2d20
Vaitul Bhayani <vaitulb@gmail.com>
2023-07-05 20:26:09 +0530

    feat: added mongoose and user and note schema - installed somepackages such as mongoose mongoose-sequence and it's @types

--- server.ts
+++ server.ts
@@ -1,15 +1,24 @@
-require("dotenv-extended").load(); // Loading environment files
+// Loading environment files
+import { load } from "dotenv-extended";
+load();
 
 // All imports should be kept here 👇
 import express from "express";
 import OTHER_ROUTES from "./routes/otherRoutes";
 import MAIN_ROUTES from "./routes/mainRoutes";
 import API_ROUTES from "./routes/apiRoutes";
-import { logger } from "./middleware/logger";
+import { logEvents, logger } from "./middleware/logger";
 import errorHandler from "./middleware/errorHandler";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import corsOptions from "./config/corsOptions";
+import connectDB from "./DB/dbConn";
+import mongoose from "mongoose";
+
+console.log("Server: Environment is: ", process.env.NODE_ENV);
+
+//Mongo Database Connection
+connectDB();
 
 //Initializing app
 const app = express();
@@ -26,8 +35,20 @@ app.use(MAIN_ROUTES);
 app.use(API_ROUTES);
 app.use(OTHER_ROUTES);
 
-//Starting server
-const port = process.env.SERVER_PORT || 3500;
-app.listen(port, () =>
-  console.log("Server is running at http://localhost:" + port)
-);
+// Mongo event listners
+mongoose.connection.once("open", () => {
+  console.info("MongoDB: Connected");
+  //Starting server
+  const port = process.env.SERVER_PORT || 3500;
+  app.listen(port, () =>
+    console.log("Server: ", "Running at http://localhost:" + port)
+  );
+});
+
+mongoose.connection.on("error", (err) => {
+  logEvents(
+    `${err.name}: ${err.code}\n${err.message}\n${err.syscall}\n${err.hostname}`,
+    "err-mongo-logs"
+  );
+  console.error(err);
+});
